437a4dea863ebdb5a1341d2fc413c31b55437cbb
Zykler <146702866+RetorcaGerard@users.noreply.github.com>
2024-08-07 11:43:05 +0800

    insertion.java

--- sorting/insertion.java
+++ sorting/insertion.java
@@ -1,7 +1,11 @@
 import java.util.*;
+
 public class insertion {
+    
     public static void main(String[] args) {
         Scanner ins = new Scanner(System.in);
+
+        
          while(true){
             System.out.println("Enter the elements (Put comma ',' between each elements: )");
             String input = ins.nextLine();
@@ -9,17 +13,21 @@ public class insertion {
             int n = elements.length;
             int[]arr = new int[n];
 
+             
             for (int i = 0; i < n; i++) {
                 arr[i] = Integer.parseInt(elements[i].trim());
             }
 
+             
             for (int i = 1 ; i < n ; i++){
                 int key = arr[i];
                 int j = i - 1;
 
+                
                 while (j >=0 && arr [j]>key){
                     arr [j+1] = arr [j];
                     j--;
+                    
                 }
 
                 arr[j+1] = key;
@@ -30,6 +38,7 @@ public class insertion {
                 }
                 System.out.println();
             }
+             
 
             System.out.println("Do you want to do bubble sort again? Y/N");
             String pick = ins.nextLine();
