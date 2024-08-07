8a50bc478b1217db590d7c0fdaa1e3a81f61a377
RetorcaGerard <146702866+RetorcaGerard@users.noreply.github.com>
2024-08-07 09:29:53 +0800

    Add files via upload

---
+++ Retorca/input.java
@@ -0,0 +1,18 @@
+//Main Encapsulation
+public class input{
+    private String name;
+    private int age;
+
+    public input(String Name, int Age){
+        this.name = Name;
+        this.age = Age;
+    }
+
+    public String getName(){
+        return name;
+    }
+    public int getAge(){
+        return age;
+    }
+}
+
---
+++ Retorca/output.java
@@ -0,0 +1,8 @@
+//output both input+Ainput
+
+public class output {
+    public static void main(String[] args) {
+        kit x = new kit();
+        x.nya();
+    }
+}
