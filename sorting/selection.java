34c5dd22a120255af3b198e4b939722bdb04bd40
RetorcaGerard <146702866+RetorcaGerard@users.noreply.github.com>
2024-08-28 07:41:47 +0800

    Update selection.java

--- sorting/selection.java
+++ sorting/selection.java
@@ -19,7 +19,7 @@ public class selection {
             for (int i = 0; i < n - 1 ; i++){
                 int minIndex = i;
                 for (int j = i + 1; j < n; j++){
-                    if (arr[j] < (minIndex)){
+                    if (arr[j] < arr[minIndex]){
                         minIndex = j;
                     }
                 }
