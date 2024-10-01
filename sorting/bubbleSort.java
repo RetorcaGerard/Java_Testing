71758f2dde259054c937f93755c8f50e6d8dfad5
RetorcaGerard <146702866+RetorcaGerard@users.noreply.github.com>
2024-10-01 08:40:47 +0800

    Bubble Sort

---
+++ sorting/bubbleSort.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class bubbleSort {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter the number of elements: ");
+        int n = scanner.nextInt();
+        
+        int[] array = new int[n];
+        
+        System.out.println("Enter the elements:");
+        for (int i = 0; i < n; i++) {
+            array[i] = scanner.nextInt();
+        }
+        
+        bubble(array);
+        
+        System.out.println("\nSorted array:");
+        printArray(array);
+        
+        scanner.close();
+    }
+    
+    public static void bubble(int[] array) {
+        int n = array.length;
+        boolean swapped;
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+            for (int j = 0; j < n - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            System.out.println("\nArray after pass " + (i + 1) + ":");
+            printArray(array);
+            if (!swapped) {
+                break;
+            }
+        }
+    }
+    
+    public static void printArray(int[] array) {
+        for (int value : array) {
+            System.out.print(value + " ");
+        }
+        System.out.println();
+    }
+}
